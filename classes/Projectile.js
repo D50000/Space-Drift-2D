5b79e32143ac8a9608499db19a8e365eeb9b01f5
Eddie Hsu <aoe102198@gmail.com>
2023-05-21 17:09:03 +0800

    Add back classes.

---
+++ classes/Projectile.js
@@ -0,0 +1,24 @@
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    // Start point
+    canvas2dContext.beginPath();
+    // arc() for drawing arc or circle.
+    canvas2dContext.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    canvas2dContext.fillStyle = this.color;
+    canvas2dContext.fill();
+  }
+
+  drawAndUpdate() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
